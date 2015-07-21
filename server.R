781a2597628d8deec1680b9d14377e9fe292f847
mostly-harmless <vnijs@ucsd.edu>
2015-07-20 21:44:46 -0700

    stateSave = TRUE

--- server.R
+++ server.R
@@ -11,6 +11,8 @@ dat <- data.frame(
 ## store state in global environment for now
 if (!exists("r_state")) r_state <- list()
 
+r_state$dataviewer_search_columns <- list("","2...8","")
+
 shinyServer(function(input, output, session) {
 	output$ui_view_vars <- renderUI({
 	  vars <- colnames(dat)
@@ -27,22 +29,25 @@ shinyServer(function(input, output, session) {
   observeEvent(input$dataviewer_search_columns, {
     isolate({
       r_state$dataviewer_search_columns <<- input$dataviewer_search_columns
-      print("r_state col search")
-      print(r_state$dataviewer_search_columns)
+      # print("r_state col search")
+      # print(r_state$dataviewer_search_columns)
     })
   })
 
-  observeEvent(input$dataviewer_search, {
-    isolate({
-      r_state$dataviewer_search <<- input$dataviewer_search
-      print("r_state global search")
-      print(r_state$dataviewer_search)
-    })
-  })
+  ## doesn't seem needed if staveState = TRUE
+  # observeEvent(input$dataviewer_search, {
+  #   isolate({
+  #     r_state$dataviewer_search <<- input$dataviewer_search
+  #     print("r_state global search")
+  #     print(r_state$dataviewer_search)
+  #   })
+  # })
 
   observeEvent(input$dataviewer_state, {
     isolate({
       r_state$dataviewer_state <<- input$dataviewer_state
+      print("r_state state")
+      print(r_state$dataviewer_state)
     })
   })
 
@@ -56,13 +61,17 @@ shinyServer(function(input, output, session) {
 		DT::datatable(dat[,input$view_vars],
 		  filter = list(position = "top"), rownames = FALSE,
 	    options = list(
-	      # stateSave = TRUE,   ## maintains state but does not show column filter settings
+	      stateSave = TRUE,   ## maintains state but does not show column filter settings
 	      searchCols = lapply(r_state$dataviewer_search_columns, mknl),
-	      search = list(search = r_state$dataviewer_search),
+	      # search = list(search = r_state$dataviewer_search),
+	      order = r_state$dataviewer_state$order,
 	      processing = FALSE
 	    )
 	  )
 	})
+
+	## yihui example
+	# search = list(search = 'Ma'), order = list(list(2, 'asc'), list(1, 'desc'))
 
   output$tbl_col_search <- renderPrint(input$dataviewer_search_columns)
   output$tbl_global_search <- renderPrint(input$dataviewer_search)
