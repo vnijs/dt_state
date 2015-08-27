5751cf98d10736743d1230a461ac49e0616905c2
mostly-harmless <vnijs@ucsd.edu>
2015-08-27 10:56:37 -0700

    Illustrate issue with minimal code

--- server.R
+++ server.R
@@ -7,7 +7,7 @@ dat <- iris
 if (!exists("r_state")) r_state <- list()
 
 ## is this used? Only from a clean start
-r_state$dataviewer_search_columns <- list("","2.5...4","","","")
+r_state$dataviewer_search_columns <- list("","2.5...4","","","[\"setosa\"]")
 
 shinyServer(function(input, output, session) {
 	output$ui_view_vars <- renderUI({
@@ -19,41 +19,27 @@ shinyServer(function(input, output, session) {
 	                 options = list(plugins = list('remove_button', 'drag_drop')))
 	})
 
-
-	getdata <- reactive({
-	  if (input$apply_filter)
-	    dat[1:10,]
-	  else
-	    dat
-	})
+# 	getdata <- reactive({
+# 	  if (input$apply_filter)
+# 	    dat[1:10,]
+# 	  else
+# 	    dat
+# 	})
 
  	## make nested list
 	mknl <- function(x) list(search = x)
 
-  observeEvent(input$dataviewer_search_columns, {
-    isolate({
-      r_state$dataviewer_search_columns <<- input$dataviewer_search_columns
-    })
-  })
-
-  observeEvent(input$dataviewer_state, {
-    isolate({
-      r_state$dataviewer_state <<- input$dataviewer_state
-    })
-  })
-
-  observeEvent(input$pivotr_search_columns, {
-    isolate({
-      r_state$pivotr_search_columns <<- input$pivotr_search_columns
-    })
-  })
-
-  observeEvent(input$pivotr_state, {
-    isolate({
-      r_state$pivotr_state <<- input$pivotr_state
-    })
-  })
-
+#   observeEvent(input$dataviewer_search_columns, {
+#     isolate({
+#       r_state$dataviewer_search_columns <<- input$dataviewer_search_columns
+#     })
+#   })
+#
+#   observeEvent(input$dataviewer_state, {
+#     isolate({
+#       r_state$dataviewer_state <<- input$dataviewer_state
+#     })
+#   })
 
   observeEvent(input$refresh, {
     r_state <<- list()
@@ -65,23 +51,24 @@ shinyServer(function(input, output, session) {
     search <- r_state$dataviewer_state$search$search
     if (is.null(search)) search <- ""
 
-		DT::datatable(getdata()[,input$view_vars],
+		# DT::datatable(getdata()[,input$view_vars],
+		DT::datatable(iris[,input$view_vars],
 		  filter = list(position = "top", clear = TRUE), rownames = FALSE,
 	    options = list(
-	      stateSave = TRUE,   ## maintains state but does not show column filter settings
-	      searchCols = lapply(r_state$dataviewer_search_columns, mknl),
-        search = list(search = search),
-	      order = r_state$dataviewer_state$order,
-	      processing = FALSE
+	      # stateSave = TRUE,   ## maintains state but does not show column filter settings
+	      searchCols = lapply(r_state$dataviewer_search_columns, mknl)
+        # search = list(search = search),
+	      # order = r_state$dataviewer_state$order,
+	      # processing = FALSE
 	    )
       ## using callback as suggested in https://github.com/rstudio/DT/issues/146
 		  # , callback = JS("$('a#refresh').on('click', function() { table.state.clear(); });")
       ## alternative callback that simplifies setting initial values
-		  , callback = DT::JS("$(window).unload(function() { table.state.clear(); })")
+		  # , callback = DT::JS("$(window).unload(function() { table.state.clear(); })")
 		)
   })
 
   output$tbl_col_search <- renderPrint(input$dataviewer_search_columns)
-  output$tbl_global_search <- renderPrint(input$dataviewer_search)
-  output$tbl_state <- renderPrint(str(input$dataviewer_state))
+  # output$tbl_global_search <- renderPrint(input$dataviewer_search)
+  # output$tbl_state <- renderPrint(str(input$dataviewer_state))
 })
--- ui.R
+++ ui.R
@@ -3,16 +3,16 @@ shinyUI(fluidPage(
     sidebarPanel(
       includeScript("www/js/jquery-ui.custom.min.js"),
       uiOutput("ui_view_vars"),
-      checkboxInput("apply_filter", "Apply filter", FALSE),
+      # checkboxInput("apply_filter", "Apply filter", FALSE),
       tags$a(id = "refresh", href = "#", class = "action-button",
              list(icon("refresh"), "Refresh"),
              onclick = "window.location.reload();")
     ),
     mainPanel(
         tabPanel("View", DT::dataTableOutput("dataviewer"),
-                 verbatimTextOutput("tbl_col_search"),
-                 verbatimTextOutput("tbl_global_search"),
-                 verbatimTextOutput("tbl_state")
+                 verbatimTextOutput("tbl_col_search")
+                 #, verbatimTextOutput("tbl_global_search")
+                 #, verbatimTextOutput("tbl_state")
       )
     )
   )
